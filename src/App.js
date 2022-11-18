f7034fc2ae0b17985e30c5f45ccfabd865cf4317
SalimBXL <el.salim.salim@gmail.com>
2022-11-18 15:31:28 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import React from 'react';
 import { useState } from 'react';
-import Box from '@mui/material/Box';
-import { prepareWordList, dateNextShowAndNextLevel, countWords } from './utils';
+import { prepareWordList, dateNextShowAndNextLevel } from './utils';
 import ToggleButtons from './components/ToggleButtons';
 import Fiche from './components/Fiche';
 import LinearWithValueLabel from './components/ProgressBar';
+import "./App.css";
+import wordsJsonFile from "./words_json.json";
 
-const words_json = {
-  20221115: [
-    { word: "word_07", translation: "mot_07", fibonacci_level: 0 },
-    { word: "word_08", translation: "mot_08", fibonacci_level: 2 },
-    { word: "word_09", translation: "mot_09", fibonacci_level: 9 },
-  ],
-  20221117: [
-    { word: "word_01", translation: "mot_01", fibonacci_level: 4 },
-    { word: "word_02", translation: "mot_02", fibonacci_level: 3 },
-    { word: "word_03", translation: "mot_03", fibonacci_level: 0 },
-  ],
-  20221119: [
-    { word: "word_04", translation: "mot_04", fibonacci_level: 5 },
-    { word: "word_05", translation: "mot_05", fibonacci_level: 1 },
-    { word: "word_06", translation: "mot_06", fibonacci_level: 6 },
-  ]
-}
-
-const reviewTypeConstants = { RECTO: "RECTO", VERSO: "VERSO", BOTH: "BOTH" };
-
+const words_json = JSON.parse(JSON.stringify(wordsJsonFile));
 
 const App = () => {
+  const reviewTypeConstants = { RECTO: "RECTO", VERSO: "VERSO", BOTH: "BOTH" };
   const [wordList, setWords] = useState(prepareWordList(words_json));
   const [reviewType, setReviewType] = useState(reviewTypeConstants.BOTH);
-  const totalWords = countWords(words_json);
   
-  return (<div>
+  return (<div className='App'>
     <h1>Vocab Trainer</h1>
 
     <ToggleButtons 
@@ -46,18 +28,7 @@ const App = () => {
 
     <LinearWithValueLabel total={wordList.length} current={0} />
 
-
-
-    <Box sx={{ 
-      display: 'flex',
-      flexWrap: 'wrap',
-      '& > :not(style)': {
-        m: 1,
-        width: 128,
-        height: 128,
-      },
-    }}>
-    
+    <div className="row ListFiches">
       {wordList.map(({word, translation, fibonacci_level}) => {
         
         const tempReviewType = (reviewType === reviewTypeConstants.BOTH) 
@@ -76,12 +47,12 @@ const App = () => {
           ? <h3>word</h3>
           : <blockquote>{translation}</blockquote>;
 
-        return <Fiche key={word} content={fiche}/>
+        return <div key={word} className="col-sm-6 col-md-4 col-lg-3">
+          <Fiche className="App-Fiche" content={fiche} />
+        </div>
         
       })}
-    </Box>
-    
-    <h3>***</h3>
+    </div>
   </div>);
 }
 
--- src/components/Fiche.jsx
+++ src/components/Fiche.jsx
@@ -1,8 +1,48 @@
 import * as React from 'react';
-import Paper from '@mui/material/Paper';
+import { useState } from 'react';
+import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import "./Fiche.css";
+
 
 export default function Fiche({content}) {
+  const [showCheckAction, setShowCheckAction] = useState(false);
+
+  const handleAnswerClick = () => setShowCheckAction(() => true);
+
+  const handlePassClick = () => {};
+
+  const handleSuccessClick = () => {};
+
+  const handleFailClick = () => {};
+
+  const showAnswerPass = () => 
+    <Stack direction="row" spacing={2}>
+      <button className="btn btn-primary" onClick={handleAnswerClick}>Answer</button>
+      <button className="btn btn-secondary" onClick={handlePassClick}>Pass</button>
+    </Stack>;
+  
+  const showSuccessError = () =>
+  <Stack direction="row" spacing={2}>
+    <button className="btn btn-success" onClick={handleSuccessClick}>Success</button>
+    <button className="btn btn-danger" onClick={handleFailClick}>Error</button>
+  </Stack>;
+
   return (
-    <Paper elevation={3}>{content}</Paper>
+    <Card className="Fiche">
+
+      <CardContent>
+        <h5>[QUESTION]</h5>
+        <blockquote>
+          {showCheckAction ? `[ANSWER]` : <br /> }
+        </blockquote>
+      </CardContent>
+
+      <CardActions>
+        {showCheckAction ? showSuccessError() : showAnswerPass()}
+      </CardActions>
+    </Card>
   );
 }
